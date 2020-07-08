22e5df885e6c94a7e7736356ef6ba2b99e2f584a
Harounnn <67702192+Harounnn@users.noreply.github.com>
2020-07-08 17:09:19 +0100

    Add files via upload

---
+++ Assets/Scripts/blocks.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blocks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int compteur = 0;
+    public int resistance = 0;
+
+    Text scoreText;
+    int Score;
+
+    void Start()
+    {
+        
+    }
+   
+
+    void OnCollisionEnter2D(Collision2D collisioninfo)
+    {
+        compteur++;
+        if(compteur == resistance)
+        {
+            Destroy(gameObject);
+        }
+
+
+        if (collisioninfo.gameObject.name == "Ball")
+        {
+            
+                //this line will just add 1 point to the score
+                Score++;
+            //this line will convert the int score variable to a string variable
+            scoreText.text = Score.ToString();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
